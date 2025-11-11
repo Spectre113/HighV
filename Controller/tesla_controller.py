0505ac6309baa5d8a3b8ade90f653f35ceabf96c
Владимир Топорков <VTvolody626@mail.ru>
2025-11-11 21:02:47 +0300

    Controller file updated: 2 cameras activation added

--- Controller/tesla_controller.py
+++ Controller/tesla_controller.py
@@ -1,10 +1,7 @@
 from controller import Robot, Camera
 import cv2
 import numpy as np
-# from follow_model_line import LineFollowerController, LineFollowerController2
-import matplotlib.pyplot as plt
-from pid_controller import PDController
-from baseline_detection import detect_yellow_lane_and_error
+from follow_model_line import LineFollowerController, LineFollowerController2
 
 
 def read_img(camera: Camera):
@@ -30,7 +27,9 @@ gps = robot.getDevice('gps')
 imu = robot.getDevice('inertial unit')
 gyro = robot.getDevice('gyro')
 lidar = robot.getDevice('lidar_on')
-camera = robot.getDevice('camera')
+camera_central = robot.getDevice('central')
+camera_right = robot.getDevice('right')
+camera_left = robot.getDevice('left')
 
 # Датчики вращения колёс
 left_rear_sensor = robot.getDevice('left_rear_sensor')
@@ -43,7 +42,9 @@ gps.enable(timestep)
 imu.enable(timestep)
 gyro.enable(timestep)
 lidar.enable(timestep)
-camera.enable(timestep)
+camera_central.enable(timestep)
+camera_right.enable(timestep)
+camera_left.enable(timestep)
 left_rear_sensor.enable(timestep)
 right_rear_sensor.enable(timestep)
 left_steer_sensor.enable(timestep)
@@ -55,19 +56,9 @@ right_motor.setPosition(float('inf'))
 left_motor.setVelocity(0.0)
 right_motor.setVelocity(0.0)
 
-
-# Setup visualization
-
-width = camera.getWidth()
-height = camera.getHeight()
-
-plt.ion()
-fig, ax = plt.subplots()
-img_display = ax.imshow(np.zeros((height, width, 3), dtype=np.uint8), cmap='gray')
-plt.show(block=False)
-
 speed = 20.0
-prev_err = None
+angle = 0
+velocity = 0.0
 
 # === ГЛАВНЫЙ ЦИКЛ ===
 while robot.step(timestep) != -1:
@@ -88,13 +79,9 @@ while robot.step(timestep) != -1:
     print(f"Lidar (первые 5): {[round(r, 2) for r in ranges[:5]]}")
 
     # ----- Камера -----
-    img = read_img(camera)
+    img = read_img(camera_central)
     height, width, _ = img.shape
     print(f"Camera: {width}x{height}")
-
-    output, err = detect_yellow_lane_and_error(img)
-
-    img_display.set_data(output)
 
     # ----- Датчики вращения -----
     print(f"Left rear wheel: {left_rear_sensor.getValue():.3f}")
@@ -106,36 +93,14 @@ while robot.step(timestep) != -1:
 
     # ----- Управление движением -----
 
-    if prev_err is None:
-        prev_err = 0.0
+    # 2nd order control
+    error_rad = 0
+    print(error_rad)
+    acceleration = LineFollowerController2().compute_steering(velocity, error_rad)
 
     dt = timestep / 1000.0
-    derr_dt = (err - prev_err) / dt
-
-    kp = 0.05
-    kd = 0.1
-
-    # Pull towards the line if too close to the edge of camera frame
-    counter = 3
-    if np.abs(err) > 0.7 and counter > 0:
-        counter -= 1
-        kp = 0.1
-        kd = 0.5
-    else:
-        counter = 3
-
-    angle = PDController(kp=kp, kd=kd).compute_steering(err, derr_dt)
-
-    prev_err = err
-
-    # 2nd order control
-    # line_angle_rad = -0.05
-    # error_rad = left_steer_sensor.getValue() - line_angle_rad
-    # acceleration = LineFollowerController2().compute_steering(velocity, error_rad)
-
-    # dt = timestep / 1000.0
-    # velocity = velocity + acceleration * dt
-    # angle = left_steer_sensor.getValue() + velocity * dt
+    velocity = velocity + acceleration * dt
+    angle = left_steer_sensor.getValue() + velocity * dt
 
     # 1st order control
     # velocity = LineFollowerController().compute_steering(error_rad)
@@ -143,13 +108,9 @@ while robot.step(timestep) != -1:
     # dt = timestep / 1000.0
     # angle = left_steer_sensor.getValue() + velocity * dt
 
-    # angle = np.clip(angle, -1, 1)
+    angle = np.clip(angle, -1, 1)
 
     left_motor.setVelocity(speed)
     right_motor.setVelocity(speed)
     left_steer.setPosition(angle)
     right_steer.setPosition(angle)
-
-    # Refresh the display
-    fig.canvas.draw()
-    fig.canvas.flush_events()
