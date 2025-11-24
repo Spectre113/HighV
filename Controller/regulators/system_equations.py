c53a61936f0f5edd2c16512f2f4b4f0d4c585cab
Ilya Grigorev <il.grigorev@innopolis.university>
2025-11-24 09:14:43 +0300

    Refactor regulators

--- Controller/regulators/system_equations.py
+++ Controller/regulators/system_equations.py
@@ -1,5 +1,3 @@
-
-
 class LineFollowerController:
     def __init__(self, k=100.0):
         self.k = k
@@ -12,11 +10,14 @@ class LineFollowerController:
 
 
 class LineFollowerController2:
-    def __init__(self, zeta = 0.01, omega_n = 100):
+    def __init__(self, zeta=0.01, omega_n=100):
         self.zeta = zeta
         self.omega_n = omega_n
 
     def compute_steering(self, steering_velocity: float, angle_err_rad: float) -> float:
-        steering_acceleration = -2 * self.zeta * self.omega_n * steering_velocity - self.omega_n ** 2 * angle_err_rad
+        steering_acceleration = (
+            -2 * self.zeta * self.omega_n * steering_velocity
+            - self.omega_n**2 * angle_err_rad
+        )
 
         return steering_acceleration
